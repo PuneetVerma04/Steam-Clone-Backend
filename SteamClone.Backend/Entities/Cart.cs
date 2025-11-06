37a3913e233583ae4a941f80069d94a1eeefbc16
Puneet Verma <verma0404puneet@gmail.com>
2025-11-07 03:48:13 +0530

    Remove Price and ImageUrl properties from CartItem class

--- SteamClone.Backend/Entities/Cart.cs
+++ SteamClone.Backend/Entities/Cart.cs
@@ -7,6 +7,4 @@ public class CartItem
     public int UserId { get; set; }
     //public required User User { get; set; }
     public int Quantity { get; set; }
-    public decimal Price => Game.Price;
-    public string? ImageUrl => Game.ImageUrl;
 }
