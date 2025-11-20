e0e80cf8b23f49488fcde48f776e620b39d7aec4
Puneet Verma <verma0404puneet@gmail.com>
2025-11-20 14:16:45 +0530

    Refactored GameResponseDTO and UpdateGameRequestDTO to remove required properties, allowing for more flexible data handling; updated property types for improved clarity.

--- SteamClone.Backend/DTOs/Game/GameResponseDTO.cs
+++ SteamClone.Backend/DTOs/Game/GameResponseDTO.cs
@@ -3,11 +3,11 @@ namespace SteamClone.Backend.DTOs.Game;
 public class GameResponseDTO
 {
     public int Id { get; set; }
-    public required string Title { get; set; }
-    public required string Description { get; set; }
-    public required decimal Price { get; set; }
-    public required string Genre { get; set; }
-    public required string Publisher { get; set; }
-    public required DateTime ReleaseDate { get; set; }
-    public required string ImageUrl { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? Genre { get; set; }
+    public string? Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string? ImageUrl { get; set; }
 }
